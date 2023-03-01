446eaaf1574db5fd29cb70fdc639382d0599d57d
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-03-01 09:01:16 +0100

    Finnished

--- Program.cs
+++ Program.cs
@@ -4,15 +4,28 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($@"Calcalutor ");
+            Console.WriteLine($@"--------------");
+
             while (true)
             {
                 mm();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("--------------------------------");
+                }
+
             }
 
         }
 
         static void mm()
         {
+            MathModel mat = new MathModel();
+
             Console.Write("Første tal : ");
             int a = int.Parse(Console.ReadLine());
 
@@ -21,6 +34,25 @@
 
             Console.Write("Anden tal : ");
             int b = int.Parse(Console.ReadLine());
+
+            switch (l)
+            {
+                case "+":
+                    Console.WriteLine(mat.Plus(a, b));
+                    break;
+                case "-":
+                    Console.WriteLine(mat.Minus(a, b));
+                    break;
+                case "*":
+                    Console.WriteLine(mat.Multiply(a, b));
+                    break;
+                case "/":
+                    Console.WriteLine(mat.Divide(a, b));
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
